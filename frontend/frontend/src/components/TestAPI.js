8649a5a71d3004c8ac58b6ee4c22b66812903fcd
Mohammedal-abdulhamid <mohammedalabdulhamid@gmail.com>
2025-09-09 11:34:33 +0100

    setup frontend react

---
+++ frontend/frontend/src/components/TestAPI.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import api from "../services/api";
+
+const TestAPI = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    //  fetching public services
+    api
+      .get("/services")
+      .then((res) => setData(res.data))
+      .catch((err) => setError(err.message));
+  }, []);
+
+  return (
+    <div>
+      <h2>API Test</h2>
+      {error && <p style={{ color: "red" }}>Error: {error}</p>}
+      {data ? (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default TestAPI;
